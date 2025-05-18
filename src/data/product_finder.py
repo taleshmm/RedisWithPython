3845603e605dfce818b428d9e8ea1fc949fd8fc3
taleshmm <taleshmm@hotmail.com>
2025-05-18 13:27:26 -0300

    feat: Implementing product_creator

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -4,10 +4,52 @@ from src.http_types.http_request import HttpRequest
 from src.http_types.http_response import HttpResponse
 
 class ProductFinder:
-    def __init__(self, redis_repo: RedisRepositoryInterface, sqlite_repo: ProductsRepositoryInterface) -> None:
+    def __init__(self, redis_repo: RedisRepositoryInterface, product_repo: ProductsRepositoryInterface) -> None:
         self.__redis_repo = redis_repo
-        self.__sqlite_repo = sqlite_repo
+        self.__product_repo = product_repo
         
     def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
-        pass
+        product_name = http_request.params["product_name"]
+        product = self.__find_in_cache(product_name)
+        if not product:
+            product = self.__find_in_sqlite(product_name)
+            self.__insert_in_cache(product)
+            
+        return self.__format_response(product)
+            
+        
+    def __find_in_cache(self, product_name: str) -> tuple:
+        product_infos = self.__redis_repo.get_key(product_name)
+        if product_infos:
+            product_infos_list = product_infos.split(",")
+            return (0, product_name, product_infos_list[0], product_infos_list[1])
+        return None
+    
+    def __find_in_sqlite(self, product_name: str) -> tuple:
+        product = self.__product_repo.find_by_name(product_name)
+        if not product: 
+            raise Exception("Product not found")
+        
+        return product
+    
+    def __insert_in_cache(self, product: tuple) -> None:
+        product_name = product[1]
+        value = f"{product[2]},{product[3]}"
+ 
+        self.__redis_repo.insert_ex(product_name, value, ex=60)
+        
+    def __format_response(self, product: tuple) -> HttpResponse:
+        return HttpResponse(
+            body={
+                "type": "PRODUCT",
+                "count": 1,
+                "attributes": {
+                "name": product[1],
+                "price": product[2],
+                "quantity": product[3]
+                }
+            },
+            status_code=200
+        )
+        
       
